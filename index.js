cd5e548b540bb1bb6b3a85762f56e5fdb480f9bd
kelvynking <kelvyn.m.king@gmail.com>
2021-09-30 14:12:13 +1000

    Defined excitedWelcomeMessage and assigned relevant string.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const currentUser = "Zero Schnauzer";
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
