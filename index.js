eda08c37f8f373a3b418586f1e360f6e18599a1a
kelvynking <kelvyn.m.king@gmail.com>
2021-09-30 14:24:06 +1000

    Defined currentUser.

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
 const currentUser = "Zero Schnauzer";
 
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-
-const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
-
-const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
